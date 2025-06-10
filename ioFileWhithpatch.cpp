09ba020707d2e244a418f6c90b26ec8ec2a61b20
Faid Arya Pamungkas <faid.arya.ft24@mail.umy.ac.id>
2025-06-10 18:17:14 +0700

    Membuka File dalam mode menulis

---
+++ ioFileWhithpatch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string namafile;
+    cout << "Masukkan Nama File : ";
+    cin >> namafile;
+    //Membuka File dalam mode menulis
+   
